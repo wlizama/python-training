e83f1a73cda94c2ab96b17cd5f16cf929b696941
Wilder Lizama <wilderlizama@gmail.com>
2018-03-06 21:59:26 -0500

    Palindromo

---
+++ palindromo.py
@@ -0,0 +1,9 @@
+def run():
+    word = str(input('Escribe una palabra: '))
+    if(word == word[::-1]): #my_str[inicio:final:saltos]
+        print('La palabra es un palindromo')
+    else:
+        print('La palabra NO es un palindromo')
+
+if __name__ == '__main__':
+    run()

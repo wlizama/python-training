b001e48e9ae083ff7354dc9c6677fad60028f40b
Wilder Lizama <wilderlizama@gmail.com>
2018-03-07 21:57:03 -0500

    Lógica de Juego ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 
 IMAGES = ['''
 
@@ -100,6 +100,34 @@ def run():
     while True:
         display_board(hidden_word, tries)
         current_letter = str(input('Escoge una letra: '))
+
+        os.system('clear') # linux o os x
+
+        letter_indexes = []
+        for idx in range(len(word)):
+            if word[idx] == current_letter:
+                letter_indexes.append(idx)
+
+        if len(letter_indexes) == 0:
+            tries += 1
+
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+
+            letter_indexes = []
+
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            break
 
 if __name__ == '__main__':
     print('B I E N V E N I D O S  A  A H O R C A D O S')

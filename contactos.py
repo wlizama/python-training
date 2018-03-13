9692c73b5ea31f8a2c9831de5381a019753cb59b
Wilder Lizama <wilderlizama@gmail.com>
2018-03-13 17:39:27 -0500

    Lógica de directorio de contactos

--- contactos.py
+++ contactos.py
@@ -1,9 +1,11 @@
+import os
+
 class Contact:
 
     def __init__(self, name, phone, email):
-        self._name = name
-        self._phone = phone
-        self._email = email
+        self.name = name
+        self.phone = phone
+        self.email = email
 
 
 class ContactBook:
@@ -12,7 +14,47 @@ class ContactBook:
         self._contacts = []
 
     def add(self, name, phone, email):
-        print('name: {}, phone: {}, email: {}'.format(name, phone, email))
+        contact = Contact(name, phone, email)
+        self._contacts.append(contact)
+
+    def show_all(self):
+        for contact in self._contacts:
+            self._print_contact(contact)
+
+    def delete(self, name):
+        for idx, contact in enumerate(self._contacts):
+            if contact.name.lower() == name.lower():
+                del self._contacts[idx]
+                break
+
+    def search(self, name):
+        for contact in self._contacts:
+            if contact.name.lower() == name.lower():
+                self._print_contact(contact)
+                break
+        else:
+            self._not_found()
+
+    def update(self, name, phone, email):
+        for contact in self._contacts:
+            if contact.name.lower() == name.lower():
+                contact.phone = phone
+                contact.email = email
+                break
+        else:
+            self._not_found()
+
+    def _print_contact(self, contact):
+        print('#############################################')
+        print('Nombre: {}'.format(contact.name))
+        print('Teléfono: {}'.format(contact.phone))
+        print('Email: {}'.format(contact.email))
+        print('#############################################')
+
+    def _not_found(self):
+        print('################################')
+        print('¡No encontrado!')
+        print('################################')
 
 
 def run():
@@ -32,28 +74,46 @@ def run():
         '''))
 
         if command == 'a':
+            os.system('clear') # linux o os x
+
             name = str(input('Escribe el nombre del contacto: '))
             phone = str(input('Escribe el tel del contacto: '))
             email = str(input('Escribe el email del contacto: '))
 
             contact_book.add(name, phone, email)
 
+            os.system('clear') # linux o os x
         elif command == 'ac':
-            print('actualizar contacto')
+            name = str(input('Escribe el nombre del contacto: '))
+            phone = str(input('Escribe el teléfono del contacto: '))
+            email = str(input('Escribe el email del contacto: '))
 
+            contact_book.update(name, phone, email)
+
+            os.system('clear') # linux o os x
         elif command == 'b':
-            print('buscar contacto')
+
+            name = str(input('Escribe el nombre del contacto: '))
+
+            contact_book.search(name)
 
         elif command == 'e':
-            print('eliminar contacto')
+            name = str(input('Escribe el nombre del contacto: '))
 
+            contact_book.delete(name)
+
+            os.system('clear') # linux o os x
         elif command == 'l':
-            print('listar contactos')
+
+            os.system('clear') # linux o os x
+
+            contact_book.show_all()
 
         elif command == 's':
             break
         else:
             print('Comando no encontrado.')
+
 
 
 if __name__ == '__main__':

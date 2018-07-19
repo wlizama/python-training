4b069937ee0a2537812d2edc0246def28da6fc52
Wilder Lizama <wilderlizama@gmail.com>
2018-07-19 11:26:43 -0500

    pdfwrite - formas, estilos y textos

--- pdfwrite/formas_estilos_textos.py
+++ pdfwrite/formas_estilos_textos.py
@@ -1,8 +1,10 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
+## Página 1
+
 w, h = A4
-c = canvas.Canvas("pdfs/formas_geometricas.pdf", pagesize=A4)
+c = canvas.Canvas("pdfs/formas_estilos_textos.pdf", pagesize=A4)
 
 """
 El método setFillColoRGB(red,green,blue) establece el color de relleno de cualquier objeto dibujado en la hoja
@@ -42,4 +44,22 @@ c.drawString(30, h - 240, "Elipse")
 c.ellipse(x, y - 170, x + 100, y - 220, fill=True)
 
 c.showPage()
+
+## Página 2
+
+"""
+Cuando cambiamos la hoja (showPage()), los estilos se pierden y ─de ser necesario─ deben ser establecidos nuevamente.
+"""
+c.drawString(30, h - 50, "Este es un texto en otra página") # no tiene los estilos de la página anterior
+
+"""
+ReportLab incluye text objects, una forma más especializada de dibujar texto.
+"""
+texto = c.beginText(50, h - 100)                              # Lugar donde empezará el texto
+texto.setFont("Courier", 20)                                  # setFont() actúa sobre el objeto en particular y no sobre el resto de la hoja.
+texto.textLine("Texto agregado con objeto texto")             # Añadimos líneas de texto a nuestro objeto.
+texto.textLine("esto es un salto de linea")
+texto.textLines("Este Texto\ntambien tiene salto de linea")   # El método textLines() soporta el carácter de salto de línea.
+c.drawText(texto)                                             # Se dibuja en la hoja
+
 c.save()

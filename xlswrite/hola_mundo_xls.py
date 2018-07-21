54d603ae82768fe612b7aef5acf772b1ae403772
Wilder Lizama <wilderlizama@gmail.com>
2018-07-20 22:56:25 -0500

    --amend

--- xlswrite/hola_mundo_xls.py
+++ xlswrite/hola_mundo_xls.py
@@ -1,5 +1,9 @@
 import xlsxwriter
 
+"""
+Al especificar un nombre de archivo, se recomienda que use una
+extensión .xlsx o Excel generará una advertencia al abrir el archivo.
+"""
 workbook = xlsxwriter.Workbook("xls/hola_mundo_xls.xlsx")
 worksheet = workbook.add_worksheet()
 

4f27f2cb4eb224870ae63a1048eee10a2d0bce10
Wilder Lizama <wilderlizama@gmail.com>
2018-08-13 22:27:03 -0500

    Decoradores - timing functions

---
+++ decoradores/decorator_timing.py
@@ -0,0 +1,25 @@
+import time
+
+def timing_function(some_function):
+    """
+    Resultados el tiempo que tarda una función ejecutar.
+    """
+    def wrapper():
+        t1 = time.time()
+        some_function()
+        t2 = time.time()
+        return "Tiempo que tardó en ejecutar la función: " + str((t2 - t1)) + "\n"
+
+    return wrapper
+
+
+@timing_function
+def my_function():
+    num_list = []
+    for num in (range(0 , 100000)):
+        num_list.append(num)
+
+    print("La suma de los números es:" + str(sum(num_list)))
+
+
+print(my_function())

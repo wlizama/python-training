e59e33763bb213f40769fcdb77434ccace35271c
Wilder Lizama <wilderlizama@gmail.com>
2018-08-22 12:54:49 -0500

    POO - Herencia

--- POO/Animal.py
+++ POO/Animal.py
@@ -1,10 +1,15 @@
 class Felino:
 
-    def es_domestico(self):
-        return True
+    def __init__(self, grito):
+        self._grito = grito
 
+    @property
     def grito(self):
-        return "zZzZzZzZz"
+        return self._grito
+
+    @grito.setter
+    def grito(self, value):
+        self._grito = value
 
     def tiene_garras(self):
         return True
@@ -14,13 +19,60 @@ class Felino:
 
 
 class Gato(Felino):
-    pass
+
+    def __init__(self, grito, es_domestico, nombre):
+        super().__init__(grito)
+        self._es_domestico = es_domestico
+        self._nombre = nombre
+
+    @property
+    def es_domestico(self):
+        return self._es_domestico
+
+    @es_domestico.setter
+    def es_domestico(self, value):
+        self._es_domestico = value
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, value):
+        self._nombre = value
+
+    def tiene_flojera(self):
+        return True
 
 
 class Leon(Felino):
-    pass
+
+    def __init__(self, grito, color_melena):
+        super().__init__(grito)
+        self._color_melena = color_melena
+
+    @property
+    def color_melena(self):
+        return self._color_melena
+
+    @color_melena.setter
+    def color_melena(self, value):
+        self._color_melena = value
 
 
-mi_gato = Gato()
-print("Mi gato es domestico {} y tiene garras {}".format(
-    mi_gato.es_domestico(), mi_gato.tiene_garras()))
+mi_gato = Gato("meaw", True, "Rasputin")
+print("Mi gato se llama: {} y cuando tiene hambre dice \"{}\"".format(
+    mi_gato.nombre, mi_gato.grito))
+print(" tiene garras: {}".format(mi_gato.tiene_garras()))
+print(" tiene colmillos: {}".format(mi_gato.tiene_colmillos()))
+print(" es domestico: {}".format(mi_gato.es_domestico))
+print(" tiene flojera: {}".format(mi_gato.tiene_flojera()))
+
+print()
+
+un_leon = Leon("grawwww", "negro")
+print("En el zoologico hay un león que tiene una melena color: {}, cuando nos \
+acercamos el grita \"{}\"".format(
+    un_leon.color_melena, un_leon.grito))
+print(" tiene garras: {}".format(un_leon.tiene_garras()))
+print(" tiene colmillos: {}".format(un_leon.tiene_colmillos()))

f44f93a39eab02077eefc0acc69f7f4014c376e3
Wilder Lizama <wilderlizama@gmail.com>
2018-08-23 22:23:22 -0500

    BBDD - Creación de tabla e inserción de datos

--- BBDD/sqliteDB01.py
+++ BBDD/sqliteDB01.py
@@ -2,6 +2,23 @@ import sqlite3
 
 micon = sqlite3.connect("DB01.db")
 cursor = micon.cursor()
-# cursor.execute()
+
+# Creación de tabla si no existe
+cursor.execute("""CREATE TABLE IF NOT EXISTS Contactos(
+Nombre VARCHAR(250),
+Apodo VARCHAR(100),
+Numero VARCHAR(10),
+Cumpleanhos VARCHAR(10)
+)""")
+
+# Inserción de valores
+cursor.execute("""INSERT INTO Contactos VALUES (
+'Wily Colon',
+'Whale colon',
+'123789456',
+'12/12'
+)""")
+
+micon.commit()
 
 micon.close()

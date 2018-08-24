a09a41f73d640fc71f232d38080a58360c8b243e
Wilder Lizama <wilderlizama@gmail.com>
2018-08-24 11:24:52 -0500

    BBDD - simple UPDATE

--- BBDD/sqliteDB01.py
+++ BBDD/sqliteDB01.py
@@ -12,7 +12,21 @@ Numero VARCHAR(10),
 Cumpleanhos VARCHAR(5)
 )""")
 
-# Inserción de valores
+
+DELIMITADOR = "*" * 50
+
+
+print(DELIMITADOR)
+print(" ANTES de operaciones")
+print(DELIMITADOR)
+cursor.execute("SELECT * FROM Contactos")
+misContactos_orig = cursor.fetchall()
+
+for contacto in misContactos_orig:
+    print(contacto)
+
+
+# # Inserción de valores
 # cursor.execute("""INSERT INTO Contactos VALUES (
 # 'Wily Colon',
 # 'Whale colon',
@@ -21,26 +35,37 @@ Cumpleanhos VARCHAR(5)
 # )""")
 
 
-# Inserción multiple
-contactos = [
-    ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
-    ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
-    ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
-    ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
-    ('Aurora Duque', 'Auro', '9584123684', '10/04'),
-    ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
-]
+# # Inserción multiple
+# contactos = [
+#     ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
+#     ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
+#     ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
+#     ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
+#     ('Aurora Duque', 'Auro', '9584123684', '10/04'),
+#     ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
+# ]
 
-cursor.executemany("""INSERT INTO Contactos VALUES (
-NULL,?,?,?,?
-)""", contactos)
+# cursor.executemany("""INSERT INTO Contactos VALUES (
+# NULL,?,?,?,?
+# )""", contactos)
+
+
+# Update
+cursor.execute("""UPDATE Contactos SET
+Apodo = 'Ferni',
+Cumpleanhos = '20/06'
+WHERE ID = 6
+""")
 
 
 # Simple Select
+print(DELIMITADOR)
+print(" DESPUES de operaciones")
+print(DELIMITADOR)
 cursor.execute("SELECT * FROM Contactos")
-misContactos = cursor.fetchall()
+misContactos_mod = cursor.fetchall()
 
-for contacto in misContactos:
+for contacto in misContactos_mod:
     print(contacto)
 
 micon.commit()

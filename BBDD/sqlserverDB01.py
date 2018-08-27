e303238129c57faaf18ab2e4c11ce422e9b46447
Wilder Lizama <wilderlizama@gmail.com>
2018-08-27 12:00:14 -0500

    BBDD - Conección a SQL Server y configuración en archivo .ini

---
+++ BBDD/sqlserverDB01.py
@@ -0,0 +1,26 @@
+"""
+Enlaces para solución de problemas
+
+https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2017#ubuntu-1404-1604-1710-and-1804
+
+https://community.webfaction.com/questions/12976/im002-im002-unixodbcdriver-managerdata-source-name-not-found-and-no-default-driver-specified-0-sqldriverconnectw/17272
+"""
+
+import pyodbc
+import configparser
+
+# detect SQL Driver
+# drivers_name = [x for x in pyodbc.drivers() if x.endswith(' for SQL Server')]
+# drivers_name[1] if drivers_name else "SQL Server"
+driver_name = "ODBC Driver 17 for SQL Server"
+
+config = configparser.ConfigParser()
+config.read("dbconfig.ini")
+
+con = pyodbc.connect("DRIVER={};SERVER={};UID={};PWD={};DATABASE={};".format(
+    driver_name,
+    config["SQL_SERVER"]["SERVER"],
+    config["SQL_SERVER"]["UID"],
+    config["SQL_SERVER"]["PWD"],
+    config["SQL_SERVER"]["DATABASE"]
+))

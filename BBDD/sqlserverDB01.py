0afcefbda5172a02b6d0fcc606008f4997035036
Wilder Lizama <wilderlizama@gmail.com>
2018-08-27 15:39:59 -0500

    BBDD - SQL Server, Delete / Update

--- BBDD/sqlserverDB01.py
+++ BBDD/sqlserverDB01.py
@@ -38,22 +38,46 @@ cursor.execute("""IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Contactos'
         cumpleanhos VARCHAR(5)
     )""")
 
+DELIMITADOR = "*" * 50
+
+print(DELIMITADOR)
+print(" ANTES de operaciones")
+print(DELIMITADOR)
+cursor.execute("SELECT * FROM Contactos")
+misContactos_orig = cursor.fetchall()
+
+for contacto in misContactos_orig:
+    print(contacto)
+
 
 # # Inserción multiple
-contactos = [
-    ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
-    ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
-    ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
-    ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
-    ('Aurora Duque', 'Auro', '9584123684', '10/04'),
-    ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
-]
+# contactos = [
+#     ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
+#     ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
+#     ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
+#     ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
+#     ('Aurora Duque', 'Auro', '9584123684', '10/04'),
+#     ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
+# ]
 
-cursor.executemany("""INSERT INTO Contactos (nombre, apodo, numero, cumpleanhos) VALUES (
-    ?,?,?,?
-    )""", contactos)
+# cursor.executemany("""INSERT INTO Contactos (nombre, apodo, numero, cumpleanhos) VALUES (
+#     ?,?,?,?
+#     )""", contactos)
+
+# # Update
+# cursor.execute("""UPDATE Contactos SET
+#     Apodo = 'Ferni',
+#     Cumpleanhos = '20/06'
+#     WHERE ID = 6
+#     """)
+
+# Delete
+cursor.execute("DELETE from Contactos WHERE ID = 6")
 
 # Simple Select
+print(DELIMITADOR)
+print(" DESPUÉS de operaciones")
+print(DELIMITADOR)
 cursor.execute("SELECT * FROM Contactos")
 misContactos_mod = cursor.fetchall()
 

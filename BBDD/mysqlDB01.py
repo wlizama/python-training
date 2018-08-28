3c5b30553f72c0ac88e7c51c1875fff3ce2976eb
Wilder Lizama <wilderlizama@gmail.com>
2018-08-28 11:40:51 -0500

    BBDD - MySQL, update / delete

--- BBDD/mysqlDB01.py
+++ BBDD/mysqlDB01.py
@@ -37,20 +37,40 @@ PRIMARY KEY (id)
 )""")
 
 
-# Inserción multiple
-contactos = [
-    ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
-    ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
-    ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
-    ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
-    ('Aurora Duque', 'Auro', '9584123684', '10/04'),
-    ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
-]
+print(DELIMITADOR)
+print(" ANTES de operaciones")
+print(DELIMITADOR)
+cursor.execute("SELECT * FROM Contactos")
+misContactos_orig = cursor.fetchall()
 
-cursor.executemany("""INSERT INTO Contactos (nombre, apodo, numero, cumpleanhos) VALUES (
-    %s, %s, %s, %s
-    )""", contactos)
+for contacto in misContactos_orig:
+    print(contacto)
 
+
+# # Inserción multiple
+# contactos = [
+#     ('Pedro Mendoza', 'Pancho Fierro', '123654789', '05/08'),
+#     ('Mary Lopez', 'Marimar', '#987456321', '03/11'),
+#     ('Enrique Altarez', 'Quique', '#989654123', '01/02'),
+#     ('Sebastian Estrada', 'Sebas', '896471235', '08/10'),
+#     ('Aurora Duque', 'Auro', '9584123684', '10/04'),
+#     ('Fernado Peralta', 'Ferd', '9236541879', '25/06')
+# ]
+
+# cursor.executemany("""INSERT INTO Contactos (nombre, apodo, numero, cumpleanhos) VALUES (
+#     %s, %s, %s, %s
+#     )""", contactos)
+
+
+# # Update
+# cursor.execute("""UPDATE contactos SET
+#     apodo = 'Ferni',
+#     cumpleanhos = '20/06'
+#     WHERE id = 6
+#     """)
+
+# Delete
+# cursor.execute("DELETE from contactos WHERE id > 6")
 
 # Simple Select
 print(DELIMITADOR)

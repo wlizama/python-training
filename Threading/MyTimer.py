24fcd00451097b2552b904780d96900ab1d938a3
Wilder Lizama <wilderlizama@gmail.com>
2019-02-20 10:59:31 -0500

    Threading_Schedulers - Simple Timer

---
+++ Threading/MyTimer.py
@@ -0,0 +1,13 @@
+from threading import Timer
+
+def sayHi(nm):
+    print("Hi with Timer arg1:", nm)
+
+
+def startTimer():
+    t = Timer(5, sayHi, args=["Wily"])
+    t.start()
+
+
+if __name__ == "__main__":
+    startTimer()

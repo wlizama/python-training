7cff4f03d6363b3f6d1f6818ad485911732a79c0
Wilder Lizama <wilderlizama@gmail.com>
2019-03-01 10:44:19 -0500

    Socket con escritura continua

--- socket/cliente01.py
+++ socket/cliente01.py
@@ -3,11 +3,16 @@ import socket
 mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 local_hostname = socket.gethostname()
+local_fqdn = socket.getfqdn()
+ip_address = socket.gethostbyname(local_hostname)
 
-mi_socket.connect((local_hostname, 31790))
+server_address = (ip_address, 31790)
+mi_socket.connect(server_address)
 
-mensaje = input("yo: ")
+print("Conneting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
 
-mi_socket.send(str(mensaje).encode("utf-8"))
+while True:
+    mensaje = input("yo: ")
+    mi_socket.sendall(str(mensaje).encode("utf-8"))
 
 mi_socket.close()
--- socket/servidor01.py
+++ socket/servidor01.py
@@ -1,17 +1,32 @@
 import socket
 
 mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 local_hostname = socket.gethostname()
+local_fqdn = socket.getfqdn()
+ip_address = socket.gethostbyname(local_hostname)
 
-mi_socket.bind((local_hostname, 31790))
-mi_socket.listen(5)
+print("Working on %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
+
+server_address = (ip_address, 31790)
+print("Starting up on %s port %s" % server_address)
+mi_socket.bind(server_address)
+
+mi_socket.listen(2)
 
 while True:
+    print("waiting for a connection")
+
     con, adrr = mi_socket.accept()
-    print("Conexion establecida {}".format(adrr))
 
-    peticion = con.recv(1024)
-    print(str(peticion))
+    try:
+        print("Connection from", adrr)
+        while True:
+            data = con.recv(64)
+            if data:
+                print("data: %s" % data)
+            else:
+                print("No more data")
+                break
+    finally:
+        con.close()
 
-    con.close()

4d92661fa89556d283201db3a1a9017a08c4d122
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-07-12 17:08:37 -0500

    checkbox ui terminal

---
+++ terminal_ui/checkbox.py
@@ -0,0 +1,55 @@
+from PyInquirer import prompt
+
+questions = [
+    {
+        'type': 'checkbox',
+        'qmark': '😃',
+        'message': 'Select toppings',
+        'name': 'toppings',
+        'choices': [ 
+            {
+                'name': 'Ham'
+            },
+            {
+                'name': 'Ground Meat'
+            },
+            {
+                'name': 'Bacon'
+            },
+            {
+                'name': 'Mozzarella',
+                'checked': True
+            },
+            {
+                'name': 'Cheddar'
+            },
+            {
+                'name': 'Parmesan'
+            },
+            {
+                'name': 'Mushroom'
+            },
+            {
+                'name': 'Tomato'
+            },
+            {
+                'name': 'Pepperoni'
+            },
+            {
+                'name': 'Pineapple'
+            },
+            {
+                'name': 'Olives',
+                'disabled': 'out of stock'
+            },
+            {
+                'name': 'Extra cheese'
+            }
+        ],
+        'validate': lambda answer: 'You must choose at least one topping.' \
+            if len(answer) == 0 else True
+    }
+]
+
+answers = prompt(questions)
+print(answers)

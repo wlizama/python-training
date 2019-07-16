9aa5b52d548aa2150125cfe65c965cc08283640a
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-07-16 17:25:55 -0500

    Pruebas 1, fail

--- pdfread/test.py
+++ pdfread/test.py
@@ -1,16 +1,49 @@
 import PyPDF2
+from pyzbar.pyzbar import decode
+from PIL import Image
+from io import BytesIO
 from pprint import pprint
 
-FILE_PDF = 'D:\\FElectronicaProveedor\\inbound\\20109969452-01-F012-00015589.PDF'
+FILE_PDF = 'C:\\Users\\Wilder\\Downloads\\20100412447-01-F005-39776.pdf'
+
 
 if __name__ == "__main__":
     filePDFReader = PyPDF2.PdfFileReader(FILE_PDF)
     pageObj =  filePDFReader.getPage(0)
 
-    xObject = pageObj['/Resources']['/XObject'].getObject()
-    for obj in xObject:
-        data = xObject[obj]._data
-        print(data)
-        # img = open("img_saved-%s.jpg" % obj.replace('/', ''), "wb")
-        # img.write(data)
-        # img.close()
+    if '/XObject' in pageObj['/Resources']:
+        xObject = pageObj['/Resources']['/XObject'].getObject()
+
+        for obj in xObject:
+            if xObject[obj]['/Subtype'] == '/Image':
+                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                data = xObject[obj]._data
+                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                    mode = "RGB"
+                else:
+                    mode = "P"
+                
+                img = None
+                
+                if '/Filter' in xObject[obj]:
+                    xfilter = xObject[obj]['/Filter']
+                    if xfilter == '/FlateDecode':
+                        img = Image.frombytes(mode, size, data)
+                        # print("here 01")
+                    elif xfilter == '/DCTDecode':
+                        img = Image.open(BytesIO(data))
+                        # print("here 02")
+                    elif xfilter == '/JPXDecode':
+                        img = Image.open(BytesIO(data))
+                        # print("here 03")
+                    elif xfilter == '/CCITTFaxDecode':
+                        img = Image.open(BytesIO(data))
+                        # print("here 04")
+                else:
+                    img = Image.frombytes(mode, size, data)
+                    # print("here 05")
+                
+                print(decode(img))
+                print(img.info)
+    else:
+        print("No image found :/")

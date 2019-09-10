baecd83eda76b59d69a592a6a9de9d6d32ac0f71
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-09-10 11:01:01 -0500

    Pruebas aleatorios

--- aleatorio.py
+++ aleatorio.py
@@ -1,9 +1,13 @@
 import random
 
+RSTART = 0
+RLIMIT = 1024
+CNUMS = 1024
 
-def run():
+
+def adivinaAleatorio():
     numberFound = False
-    randomNumber = random.randint(0, 20)
+    randomNumber = random.randint(RSTART, RLIMIT)
 
     while not numberFound:
         numberInput = int(input("intenta un número: "))
@@ -17,5 +21,39 @@ def run():
             print("El número es mas grande")
 
 
+def randonList():
+    rnd_list = [random.randint(RSTART, RLIMIT) for i in range(CNUMS)]
+    print(rnd_list)
+
+
+def randonListFnrandrange():
+    rnd_list = [random.randrange(RSTART, RLIMIT) for i in range(CNUMS)]
+    print(rnd_list)
+
+
+def randonListNoRepeat():
+    rnd_list = random.sample(range(RSTART, RLIMIT), CNUMS)
+    print(rnd_list)
+
+
 if __name__ == '__main__':
-    run()
+    option = int(input("""
+    Prueba de %i números aleatorios generados desde %i hasta %i
+    Seleccionar opcion:
+    
+    [1] Prueba adivinar aleatorio
+    [2] Imprimir lista de aleatorios
+    [3] Imprimir lista de aleatorios con funcion 'randrange'
+    [4] Imprimir lista de aleatorios sin repetir
+    """ % (CNUMS, RSTART, RLIMIT)))
+
+    if option == 1:
+        adivinaAleatorio()
+    elif option == 2:
+        randonList()
+    elif option == 3:
+        randonListFnrandrange()
+    elif option == 4:
+        randonListNoRepeat()
+    else:
+        print("No existe opción")

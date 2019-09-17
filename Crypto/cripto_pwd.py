b428b154b3cd20dfbc59fcae9ff6348fa0885da9
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-09-17 17:57:02 -0500

    Pasword Encrypt

---
+++ Crypto/cripto_pwd.py
@@ -0,0 +1,22 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+pwd = b"password" #input("Ingrese contraseña: ")
+salt = os.urandom(16)
+
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64decode(kdf.derive(pwd)) #.encode("UTF-8")
+f = Fernet(key)
+token = f.encrypt(b"message")
+print(token)
